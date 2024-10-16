970b1a6482f88ebff670eafa544d8136ee0f8c29
owenfunke <owen.funke@gmail.com>
2024-10-15 22:32:00 -0500

    Working Vignette

--- Scenes/Vignette.cs
+++ Scenes/Vignette.cs
@@ -3,16 +3,22 @@ using System;
 
 public partial class Vignette : Sprite2D
 {
+	private Ball _ball;
 	public float alpha =  0.5f;
+	 private float targetAlpha = 0.5f;
+    private float lerpSpeed = 0.1f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_ball = GetNode<Ball>("../MyBalls");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		/* alpha = (GetNode<Ball>("../Ball").speed - 500)/20;
-		Modulate = new Color(Modulate.R , Modulate.G , Modulate.B , alpha); */
+		
+		alpha = (_ball.speed - 500) / 500;
+		alpha = Mathf.Lerp(alpha, targetAlpha, lerpSpeed);
+		Modulate = new Color(Modulate.R , Modulate.G , Modulate.B , alpha);
 	}
 }
